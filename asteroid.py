2cd496cc66347ab93b0fa8633f82ff6584a3be63
Milkftw <282414362+Milkftw@users.noreply.github.com>
2026-05-21 20:15:56 -0700

    Add split functionality to asteroids enabling full gameplay loop

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -12,4 +14,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
-        
+
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        asteroid_1_mv = self.velocity.rotate(angle)
+        asteroid_2_mv = self.velocity.rotate(-angle)
+        new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+        asteroid_1.velocity = asteroid_1_mv * 1.2
+        asteroid_2.velocity = asteroid_2_mv * 1.2
